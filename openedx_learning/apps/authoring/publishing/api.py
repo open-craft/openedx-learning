c9712955a8a1fafbd45e19b9bb40afa735c7c3cb
Braden MacDonald <braden@opencraft.com>
2025-06-30 15:56:32 +0200

    perf: more container select_related optimizations, plus test

--- openedx_learning/apps/authoring/publishing/api.py
+++ openedx_learning/apps/authoring/publishing/api.py
@@ -1182,7 +1182,12 @@ def get_container(pk: int) -> Container:
     return Container.objects.get(pk=pk)
 
 
-def get_container_by_key(learning_package_id: int, /, key: str) -> Container:
+def get_container_by_key(
+    learning_package_id: int,
+    /,
+    key: str,
+    select_related: list[str] | None = None,
+) -> Container:
     """
     [ 🛑 UNSTABLE ]
     Get a container by its learning package and primary key.
@@ -1194,7 +1199,10 @@ def get_container_by_key(learning_package_id: int, /, key: str) -> Container:
     Returns:
         The container with the given primary key.
     """
-    return Container.objects.get(
+    containers = Container.objects
+    if select_related:
+        containers = containers.select_related(*select_related)
+    return containers.get(
         publishable_entity__learning_package_id=learning_package_id,
         publishable_entity__key=key,
     )
@@ -1275,6 +1283,7 @@ def get_entities_in_container(
     container: Container,
     *,
     published: bool,
+    select_related_version: str | None = None,
 ) -> list[ContainerEntityListEntry]:
     """
     [ 🛑 UNSTABLE ]
@@ -1285,24 +1294,36 @@ def get_entities_in_container(
         container: The Container, e.g. returned by `get_container()`
         published: `True` if we want the published version of the container, or
             `False` for the draft version.
+        select_related_version: An optional optimization; specify a relationship
+        on ContainerVersion, like `componentversion` or `containerversion__x`
+        to preload via select_related.
     """
     assert isinstance(container, Container)
-    container_version = container.versioning.published if published else container.versioning.draft
+    if published:
+        # Very minor optimization: reload the container with related 1:1 entities
+        container = Container.objects.select_related(
+            "publishable_entity__published__version__containerversion__entity_list",
+        ).get(pk=container.pk)
+        container_version = container.versioning.published
+        select_related = ["entity__published__version"]
+        if select_related_version:
+            select_related.append(f"entity__published__version__{select_related_version}")
+    else:
+        # Very minor optimization: reload the container with related 1:1 entities
+        container = Container.objects.select_related(
+            "publishable_entity__draft__version__containerversion__entity_list",
+        ).get(pk=container.pk)
+        container_version = container.versioning.draft
+        select_related = ["entity__draft__version"]
+        if select_related_version:
+            select_related.append(f"entity__draft__version__{select_related_version}")
     if container_version is None:
         raise ContainerVersion.DoesNotExist  # This container has not been published yet, or has been deleted.
     assert isinstance(container_version, ContainerVersion)
     entity_list = []
     for row in container_version.entity_list.entitylistrow_set.select_related(
         "entity_version",
-        "entity_version__componentversion",
-        "entity__published__version",
-        "entity__published__version__componentversion",
-        "entity__published__version__containerversion__unitversion",
-        "entity__published__version__containerversion__subsectionversion",
-        "entity__draft__version",
-        "entity__draft__version__componentversion",
-        "entity__draft__version__containerversion__unitversion",
-        "entity__draft__version__containerversion__subsectionversion",
+        *select_related,
     ).order_by("order_num"):
         entity_version = row.entity_version  # This will be set if pinned
         if not entity_version:  # If this entity is "unpinned", use the latest published/draft version:
